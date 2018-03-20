6d4e2bfa55330b1db89c2b40bd188dd345804dec
Maina <maina@localhost.localdomain>
2018-03-20 14:00:41 +0300

    Deploy setting update

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,13 +11,13 @@ set :repo_url, "git@github.com:kihuyu/finblog.git"
 
 #require 'bundler/capistrano'
 # Default deploy_to directory is /var/www/my_app_name
-set :rvm_type, :system
-set :rvm_ruby_version, 'ruby-2.4.2'
+set :rvm_type, :user
+set :rvm_ruby_version, '2.4.2@rails5'
 
 set :deploy_to, "/var/www/finblog"
 #set :use_sudo, true
 set :user, "ubuntu"
-set :rvm_bin_path, "$HOME/.rvm/bin"
+#set :rvm_bin_path, "$HOME/.rvm/bin"
 set :default_env, { rvm_bin_path: '~/.rvm/bin' }
 set :branch, 'master'
 set :pty, true
@@ -25,17 +25,17 @@ set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 append :linked_dirs, '.bundle'
 set :keep_releases, 5
-set :rvm_map_bins, %w{rake gem bundle ruby}
-set :bundle_roles, :all                                         # this is default
-set :bundle_servers, -> { release_roles(fetch(:bundle_roles)) } # this is default
-set :bundle_binstubs, -> { shared_path.join('bin') }            # default: nil
-set :bundle_gemfile, -> { release_path.join('MyGemfile') }      # default: nil
-set :bundle_path, -> { shared_path.join('bundle') }             # this is default. set it to nil for skipping the --path flag.
-set :bundle_without, %w{development test}.join(' ')             # this is default
-set :bundle_flags, '--deployment --quiet'                       # this is default
-set :bundle_env_variables, {}                                   # this is default
-set :bundle_clean_options, ""                                   # this is default. Use "--dry-run" if you just want to know what gems would be deleted, without actually deleting them
-set :rvm1_map_bins, fetch(:rvm1_map_bins).to_a.concat(%w(sidekiq sidekiqctl))
+#set :rvm_map_bins, %w{rake gem bundle ruby}
+#set :bundle_roles, :all                                         # this is default
+#set :bundle_servers, -> { release_roles(fetch(:bundle_roles)) } # this is default
+#set :bundle_binstubs, -> { shared_path.join('bin') }            # default: nil
+#set :bundle_gemfile, -> { release_path.join('MyGemfile') }      # default: nil
+#set :bundle_path, -> { shared_path.join('bundle') }             # this is default. set it to nil for skipping the --path flag.
+#set :bundle_without, %w{development test}.join(' ')             # this is default
+#set :bundle_flags, '--deployment --quiet'                       # this is default
+#set :bundle_env_variables, {}                                   # this is default
+#set :bundle_clean_options, ""                                   # this is default. Use "--dry-run" if you just want to know what gems would be deleted, without actually deleting them
+#set :rvm1_map_bins, fetch(:rvm1_map_bins).to_a.concat(%w(sidekiq sidekiqctl))
 #set :rbenv_type, :user
 #set :default_env, { rvm_bin_path: '~/.rvm/bin' }
 #set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
