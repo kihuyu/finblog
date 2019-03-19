92a3ceeab40010fd80b3947a9da0e8dfa761ab11
Maina <mainakihuyu@gmail.com>
2019-03-19 11:23:01 +0300

    FS-111: Test2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include ActionController::MimeResponds
+  include Sub::SubSub
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
