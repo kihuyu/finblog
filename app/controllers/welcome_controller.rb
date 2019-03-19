328bd63fc4030c499a92fbde23faabd1a0d0775a
Maina <mainakihuyu@gmail.com>
2019-03-19 11:26:38 +0300

    Test3

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    #rs = Sub::SubSub.new({test: 2})
+    #if rs.authenticated?
+    puts TEST
+    if Sub::SubSub.authenticated?
+      puts '1'
+    else
+      puts '2'
+    end
   end
 end
